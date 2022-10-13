a8382412769719477f01ba7a2e12ca750f3f190f
Kim gyeong su <sk62628504@gmail.com>
2022-10-13 23:23:57 +0900

    springboot

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import com.example.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -19,6 +20,7 @@ public class BoardController {
     }
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board){
+
         boardService.write(board);
         return "";
     }
