bd04290bf7c258846bcd63adfbb95130a796b789
Kim gyeong su <sk62628504@gmail.com>
2022-10-14 11:07:26 +0900

    springboot

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.example.board.entity.Board;
 import com.example.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,5 +24,11 @@ public class BoardController {
 
         boardService.write(board);
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list",boardService.boardList());
+        return "boardlist";
     }
 }
